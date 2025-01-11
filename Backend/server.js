5be8a29fb7cf3c18ef835a94dfac8768ccacced2
Benson Zhou <bz2509@g.rit.edu>
2025-01-10 19:41:41 -0500

    port fix in backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 })
